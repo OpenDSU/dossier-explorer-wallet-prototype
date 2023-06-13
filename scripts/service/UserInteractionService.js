74161735a513a64b8cfca328f3adffd8a3e54d0a
Teodor Lupu <luputeodor2@gmail.com>
2023-06-13 12:06:04 +0300

    added different types of toast messages, refactor code from UserInteractionService

--- scripts/service/UserInteractionService.js
+++ scripts/service/UserInteractionService.js
@@ -9,9 +9,65 @@ class UserInteractionService {
         toastAlert.setAttribute("aria-atomic", "true");
         // toastAlert.setAttribute("data-autohide", "false");
         toastAlert.setAttribute("data-delay", "300");
-        toastAlert.classList.add("toast");
+        toastAlert.classList.add("error-toast");
 
-        toastAlert.innerHTML = "  <div class=\"toast-header\">\n" +
+        this.addToastMessage(toastAlert, title, args);
+
+        document.getElementById("toasts").appendChild(toastAlert);
+        toastAlert.classList.add("show");
+        this.addToastEventListeners(toastAlert);
+    }
+
+    showAlert(title, ...args) {
+        let toastAlert = document.createElement("div");
+        toastAlert.setAttribute("role", "alert");
+        toastAlert.setAttribute("aria-live", "assertive");
+        toastAlert.setAttribute("aria-atomic", "true");
+        // toastAlert.setAttribute("data-autohide", "false");
+        toastAlert.setAttribute("data-delay", "300");
+        toastAlert.classList.add("alert-toast");
+
+        this.addToastMessage(toastAlert, title, args);
+
+        document.getElementById("toasts").appendChild(toastAlert);
+        toastAlert.classList.add("show");
+        this.addToastEventListeners(toastAlert);
+    }
+
+    showInfo(title, ...args) {
+        let toastAlert = document.createElement("div");
+        toastAlert.setAttribute("role", "alert");
+        toastAlert.setAttribute("aria-live", "assertive");
+        toastAlert.setAttribute("aria-atomic", "true");
+        // toastAlert.setAttribute("data-autohide", "false");
+        toastAlert.setAttribute("data-delay", "300");
+        toastAlert.classList.add("info-toast");
+
+        this.addToastMessage(toastAlert, title, args);
+
+        document.getElementById("toasts").appendChild(toastAlert);
+        toastAlert.classList.add("show");
+        this.addToastEventListeners(toastAlert);
+    }
+
+    showConfirm(title, ...args) {
+        let toastAlert = document.createElement("div");
+        toastAlert.setAttribute("role", "alert");
+        toastAlert.setAttribute("aria-live", "assertive");
+        toastAlert.setAttribute("aria-atomic", "true");
+        // toastAlert.setAttribute("data-autohide", "false");
+        toastAlert.setAttribute("data-delay", "300");
+        toastAlert.classList.add("confirm-toast");
+
+        this.addToastMessage(toastAlert, title, args);
+
+        document.getElementById("toasts").appendChild(toastAlert);
+        toastAlert.classList.add("show");
+        this.addToastEventListeners(toastAlert);
+    }
+
+    addToastMessage(toast, title, ...args) {
+        toast.innerHTML = "  <div class=\"toast-header\">\n" +
             "    <strong class=\"mr-auto\">" + title + "</strong>\n" +
             "    <button type=\"button\" class=\"ml-2 mb-1 close\" data-dismiss=\"toast\" aria-label=\"Close\">\n" +
             "      <span aria-hidden=\"true\">&times;</span>\n" +
@@ -20,20 +76,18 @@ class UserInteractionService {
             "  <div class=\"toast-body\">\n" +
             args +
             "  </div>\n"
+    }
 
-        document.getElementById("toasts").appendChild(toastAlert);
-        toastAlert.classList.add("show");
-
-        toastAlert.querySelector('.close').addEventListener('click', (model, target, event) => {
+    addToastEventListeners(toast) {
+        toast.querySelector('.close').addEventListener('click', (model, target, event) => {
             if (!event) {
                 //this is click event from file type inputs
                 event = model;
             }
-            toastAlert.classList.remove("show");
-            toastAlert.classList.add("hide");
+            toast.classList.remove("show");
+            toast.classList.add("hide");
         })
     }
-
 }
 
 let instance = new UserInteractionService();
